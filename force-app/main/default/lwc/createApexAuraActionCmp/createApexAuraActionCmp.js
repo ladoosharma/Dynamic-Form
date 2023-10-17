65f62bbf581b493fc08696f82a645526fa46ade7
Gaurav <gaurav.sharma@inflooens.com>
2023-10-17 16:30:26 +0530

    merge field cmp

--- force-app/main/default/lwc/createApexAuraActionCmp/createApexAuraActionCmp.js
+++ force-app/main/default/lwc/createApexAuraActionCmp/createApexAuraActionCmp.js
@@ -1,68 +1,122 @@
-import { LightningElement, api, wire } from 'lwc';
-import getActionRecord from '@salesforce/apex/DynamicFormCreatorController.getActionRecord';
-import { updateRecord, createRecord } from 'lightning/uiRecordApi';
+import { LightningElement, api, wire, track } from "lwc";
+import getActionRecord from "@salesforce/apex/DynamicFormCreatorController.getActionRecord";
+//import { updateRecord, createRecord } from "lightning/uiRecordApi";
 export default class CreateApexAuraActionCmp extends LightningElement {
-    @api actionId;
-    @api formObject;
-    @api isFieldAction = false;
-    @api isSectionAction = false;
-    @api isTemplateAction = false;
-    whereClauseForAction;
-    relatedFieldLabel;
-    relatedActionObject;
-    relatedObjectId;
-    formActionData;
-    openNewActionPopup;
-    connectedCallback(){
-        this.whereClauseForAction = ` AND Object_Related_To__c = '${this.formObject}'`;
-        this.relatedActionObject = (this.isFieldAction)?'Form_Field__c':this.isSectionAction?'Form_Section__c':this.isTemplateAction?'Form_Template__c':'';
-        this.relatedFieldLabel = (this.isFieldAction)?'Field Related to':this.isSectionAction?'Section Related to':this.isTemplateAction?'Template Related to':'';
-        if(!this.actionId){
-            this.formActionData = {};
+  @api actionId;
+  @api formObject;
+  @api isFieldAction = false;
+  @api isSectionAction = false;
+  @api isTemplateAction = false;
+  whereClauseForAction;
+  relatedFieldLabel;
+  relatedActionObject;
+  lookupFieldApiName;
+  relatedObjectId;
+  @track formActionData;
+  openNewActionPopup;
+  connectedCallback() {
+    this.whereClauseForAction = ` AND Object_Related_To__c = '${this.formObject}'`;
+    this.relatedActionObject = this.isFieldAction
+      ? "Form_Field__c"
+      : this.isSectionAction
+      ? "Form_Section__c"
+      : this.isTemplateAction
+      ? "Form_Template__c"
+      : "";
+    this.lookupFieldApiName = this.isFieldAction
+      ? "Field_Action__c"
+      : this.isSectionAction
+      ? "Section_Action__c"
+      : this.isTemplateAction
+      ? "Form_Template__c"
+      : "";
+    this.relatedFieldLabel = this.isFieldAction
+      ? "Field Related to"
+      : this.isSectionAction
+      ? "Section Related to"
+      : this.isTemplateAction
+      ? "Template Related to"
+      : "";
+    if (!this.actionId) {
+      this.formActionData = {};
+    }
+  }
+  @wire(getActionRecord, { actionId: "$actionId" })
+  actionFetch({ data, error }) {
+    if (data) {
+      this.formActionData = data;
+      this.relatedActionObject = data.Field_Action__c
+        ? "Form_Field__c"
+        : data.Section_Action__c
+        ? "Form_Section__c"
+        : data.Form_Template__c
+        ? "Form_Template__c"
+        : "";
+      this.relatedFieldLabel = data.Field_Action__c
+        ? "Field Related to"
+        : data.Section_Action__c
+        ? "Section Related to"
+        : data.Form_Template__c
+        ? "Template Related to"
+        : "";
+      this.relatedObjectId = data.Field_Action__c
+        ? data.Field_Action__c
+        : data.Section_Action__c
+        ? data.Section_Action__c
+        : data.Form_Template__c
+        ? data.Form_Template__c
+        : "";
+    }
+    if (error) {
+      //handle error here
+    }
+  }
+  lookupRecord(evt) {
+    const recordSelected = evt.detail.selectedRecord;
+    const fieldToBeMappedWith = evt.target.dataset.id;
+    if (fieldToBeMappedWith) {
+      this.formActionData[fieldToBeMappedWith] = recordSelected
+        ? recordSelected.Id
+        : "";
+    }
+    if (fieldToBeMappedWith === "Apex_Aura_Action__c" && recordSelected) {
+      this.formActionData.Argument_Mapping__c =
+        recordSelected.additionalFields.find((e) => {
+          return e.apiName === "Argument_List__c";
+        }).value;
+    } else if (fieldToBeMappedWith === "Apex_Aura_Action__c") {
+      this.formActionData.Argument_Mapping__c = "";
+    }
+  }
+  submitform() {
+    if (this.actionId) {
+      /**update the record , or create a new one */
+    } else {
+      /** create new record */
+    }
+  }
+  createNewAction() {
+    this.openNewActionPopup = true;
+  }
+  handleClose() {
+    this.openNewActionPopup = false;
+  }
+  handleSubmit() {
+    this.template
+      .querySelector("c-create-new-dynamic-action")
+      .submitForm()
+      .then((data) => {
+        if (data) {
+          this.formActionData.Apex_Aura_Action__c = data.id;
+          this.formActionData.Argument_Mapping__c =
+            data.fields.Argument_List__c.value;
+          this.openNewActionPopup = false;
         }
-    }
-    @wire(getActionRecord, {actionId: '$actionId'})
-    actionFetch({data, error}){
-        if(data){
-            this.formActionData = data;
-            this.relatedActionObject = (data.Field_Action__c)?'Form_Field__c':data.Section_Action__c?'Form_Section__c':data.Form_Template__c?'Form_Template__c':'';
-            this.relatedFieldLabel = (data.Field_Action__c)?'Field Related to':data.Section_Action__c?'Section Related to':data.Form_Template__c?'Template Related to':'';
-            this.relatedObjectId = (data.Field_Action__c)?data.Field_Action__c:data.Section_Action__c?data.Section_Action__c:data.Form_Template__c?data.Form_Template__c:'';
-        }
-        if(error){
-            //handle error here
-        }
-    }
-    lookupRecord(evt){
-        const target = evt.target;
-        
-    }
-    submitform(){
-        if(this.actionId){
-            /**update the record , or create a new one */
-        }else{
-
-        }
-    }
-    createNewAction(){
-        this.openNewActionPopup = true;
-    }
-    handleClose(){
+      })
+      .bind(this)
+      .catch((error) => {
+        console.error(error);
         this.openNewActionPopup = false;
-    }
-    handleSubmit(){
-        const actioncmp = this.template.querySelector('c-custom-lookup-lwc[data-id="actionName"]');
-        this.template.querySelector('c-create-new-dynamic-action').submitForm()
-        .then((data) => {
-            if(actioncmp){
-                actioncmp.defaultRecordId = data.id;
-                this.openNewActionPopup = false;
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            this.openNewActionPopup = false;
-        })
-    }
-
+      });
+  }
 }
--- force-app/main/default/lwc/mergeFieldCreatorCmp/mergeFieldCreatorCmp.js
+++ force-app/main/default/lwc/mergeFieldCreatorCmp/mergeFieldCreatorCmp.js
@@ -1,6 +1,205 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire, track } from "lwc";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
+const NONE_PICK = { label: "--None--", value: "" };
 
 export default class MergeFieldCreatorCmp extends LightningElement {
-    @api objectRelatedTo;
-    
+  @api objectRelatedTo;
+  currentLevel = 1;
+  tempObj;
+  @api argumetList;
+  @track currentObjectInfo;
+  @track currentFieldsIteration;
+  @track tempCurrentFieldIteration;
+  @track currentFieldPath;
+  isChildRelationSelected = false;
+  hideLookupPopup = true;
+  connectedCallback() {
+    this.tempObj = this.objectRelatedTo;
+    this.currentFieldPath = [
+      {
+        currentObject: this.objectRelatedTo,
+        label: "$Record",
+        fieldPath: "$Record",
+        currentlySelected: false
+      }
+    ];
+  }
+  @wire(getObjectInfo, { objectApiName: "$tempObj" })
+  objectInfoResult({ data, error }) {
+    if (data) {
+      this.currentObjectInfo = data;
+      this.handleFieldCreationList();
+    }
+    if (error) {
+      /**handle error here */
+    }
+  }
+  handleFieldCreationList() {
+    if (this.currentObjectInfo) {
+      const fields = this.currentObjectInfo.fields;
+      this.currentFieldsIteration = [
+        ...Object.keys(fields).map((eachField) => {
+          return {
+            apiName: eachField,
+            label: fields[eachField].label,
+            isParentRelation: fields[eachField].dataType === "Reference",
+            relatedToObject: fields[eachField].referenceToInfos.length
+              ? fields[eachField].referenceToInfos[0].apiName
+              : "",
+            relationShipType:
+              fields[eachField].dataType === "Reference" ? "Parent" : "Field",
+            iconName:
+              fields[eachField].dataType === "Reference"
+                ? "utility:add_source"
+                : "utility:formula"
+          };
+        }),
+        ...(!this.isChildRelationSelected
+          ? this.currentObjectInfo.childRelationships.map((eachChild) => {
+              return {
+                label: eachChild.relationshipName,
+                apiName: eachChild.relationshipName,
+                relatedToObject: eachChild.eachChild,
+                relationShipType: "Child",
+                isChildRelation: true,
+                iconName: "utility:hierarchy"
+              };
+            })
+          : [])
+      ];
+      this.tempCurrentFieldIteration = this.currentFieldsIteration;
+    }
+  }
+  handleSelection(evt) {
+    const selectedApiName = evt.target.dataset.field
+      ? evt.target.dataset.field
+      : evt.currentTarget.dataset.field;
+    const isFieldSelected = evt.target.dataset.selected
+      ? evt.target.dataset.selected
+      : evt.currentTarget.dataset.selected;
+    evt.stopPropagation();
+    if (selectedApiName) {
+      const selectedField = this.currentFieldsIteration.find((field) => {
+        return field.apiName === selectedApiName;
+      });
+      if (selectedField) {
+        if (selectedField.isChildRelation) {
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].fieldPath += `[${selectedField.apiName}]`;
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].label += `-->[${selectedField.apiName}]`;
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].currentlySelected = true;
+        } else if (selectedField.isParentRelation) {
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].fieldPath += `.${selectedField.apiName}`;
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].label += `-->${selectedField.label}`;
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].currentlySelected = true;
+        } else {
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].fieldPath += `.${selectedField.apiName}`;
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].label += `-->${selectedField.apiName}`;
+          this.currentFieldPath[
+            this.currentFieldPath.length - 1
+          ].currentlySelected = true;
+          /** hide the lookup field if the field is selected */
+        }
+      }
+      if (isFieldSelected === "false") {
+        /** put inside a list and then move the ietration forward */
+        this.currentFieldPath.push({
+          currentObject: selectedField.relatedToObject,
+          label: this.currentFieldPath[this.currentFieldPath.length - 1].label,
+          fieldPath:
+            this.currentFieldPath[this.currentFieldPath.length - 1].fieldPath,
+          currentlySelected: false
+        });
+        this.isChildRelationSelected = selectedField.isChildRelation;
+        if (this.tempObj !== selectedField.relatedToObject) {
+          this.tempObj = selectedField.relatedToObject;
+          this.currentObjectInfo = undefined;
+        }
+      } else {
+        /** close the lookup and populate the path value and append with Argument */
+        this.hideLookupPopup = true;
+      }
+    }
+  }
+  openDropDown(evt) {
+    const lookupInputContainer = this.template.querySelector(
+      ".lookupInputContainer"
+    );
+    const clsList = lookupInputContainer.classList;
+    const whichEvent = evt.target.getAttribute("data-source");
+    switch (whichEvent) {
+      case "searchInputField":
+        clsList.add("slds-is-open");
+        break;
+      case "lookupContainer":
+        clsList.remove("slds-is-open");
+        break;
+      default:
+        break;
+    }
+  }
+  removeSelection(evt) {
+    const indexValue = evt.target.dataset.index;
+    if (indexValue !== undefined) {
+      this.hideLookupPopup = false;
+      this.currentFieldPath = this.currentFieldPath.slice(
+        0,
+        parseInt(indexValue, 10) - 1
+      );
+      this.currentFieldPath[
+        this.currentFieldPath.length - 1
+      ].currentlySelected = false;
+    }
+  }
+  get argsList() {
+    /** here split the args and make it as picklist */
+    if (this.argumetList) {
+      const argumentRegex = /(?:,\s*)?(\w+)(?=\s*(?:,|\)))/g;
+      const argumentNames = this.argumetList.match(argumentRegex);
+      return [
+        NONE_PICK,
+        ...argumentNames.map((e) => {
+          return { label: e, value: e };
+        })
+      ];
+    }
+    return [NONE_PICK];
+  }
+
+  handleLookupSelection(evt) {
+    const relationShip = evt.target.value;
+    if (relationShip) {
+      /** handle relationship selection */
+    }
+  }
+  enableLookup(evt) {
+    this.hideLookupPopup = evt.target.value === "";
+  }
+  searchLookup(evt) {
+    const value = evt.target.value;
+    if (value.length > 3) {
+      this.tempCurrentFieldIteration = this.currentFieldsIteration.filter(
+        (eachField) => {
+          return (
+            eachField.apiName.includes(value) || eachField.label.includes(value)
+          );
+        }
+      );
+    }
+  }
 }
